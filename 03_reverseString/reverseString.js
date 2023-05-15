7fb02b56fd462167dbe583b110f64ff5d50a03c6
enpeen <enpeen0@gmail.com>
2023-05-15 19:21:21 +0200

    Finish exercise reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let n = string.length;
+    let result = "";
 
+    for (let i = n; i > 0; i--) {
+        result += string.slice(i - 1, i)
+    }
+
+    return result;
 };
 
 // Do not edit below this line
