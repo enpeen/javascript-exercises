2ff4a0b4a8262ab83fe0d507f5d9966deec5087e
enpeen <enpeen0@gmail.com>
2023-05-17 05:12:31 +0200

    Finish removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,33 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let argsArrayed = Array.from(args);
 
+    for (let argArrayed of argsArrayed) {
+        if (array.includes(argArrayed)) {
+            let itemIndex = array.indexOf(argArrayed);
+            delete array[itemIndex];
+        }
+    }
+    
+    arrayFiltered = array.filter(function(number) {
+        if (number != undefined) {
+            return number;
+        };
+    });
+
+    return arrayFiltered;
 };
+
+/*
+const removeFromArray = function(array, number1, number2) {
+    if (number1 != null && array.includes(number1)) {
+        array.splice(number1 - 1, 1);
+    }
+    if (number2 != null && array.includes(number2)) {
+        array.splice(number2 - 1, 1);
+    }
+    return array;
+};
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
