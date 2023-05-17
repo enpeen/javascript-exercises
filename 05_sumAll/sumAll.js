d36c426e79d604ba5bf8b287d981bbd631ba207a
enpeen <enpeen0@gmail.com>
2023-05-17 07:23:03 +0200

    Finished sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let result = 0;
+    if ((x < 0 || typeof(x) != "number") || (y < 0 || typeof(y) != "number")) {
+        result = "ERROR";
+    } else if (x < y) {
+        for (let i = x; i <= y; i++) {
+            result += i;
+        }
+    } else if (x > y) {
+        for (let i = y; i <= x; i++) {
+            result += i;
+        }
+    } 
+    return result;
 };
 
 // Do not edit below this line
