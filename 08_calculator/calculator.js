729705dc0ff22243575a625e1148ec249ba523a2
enpeen <enpeen0@gmail.com>
2023-06-28 17:24:29 +0200

    Calculator completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,57 @@
-const add = function() {
-	
+const add = function(a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    let result;
+    if (array.length ==  0) {
+        result = 0;
+        return result;
+    } else {
+        array.reduce(function(sum, value) {
+        result = sum + value;
+        return result;
+        }, 0);
+    }
+    return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+    let result;
+    array.reduce(function(sum, value) {
+        result = sum * value;
+        return result;
+    });
+    return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+    let result = a;
+    if (b == 1) {
+        return result;
+    }
+    for (let i = 2; i <= b; i++) {
+        result = result * a;
+    }
+    console.log(result);
+    return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+    let result = a;
+    if (result == 0) {
+        result = 1;
+        return result;
+    } else {
+        for (let i = a - 1; i >= 2; i--) {
+            result = result * i;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
