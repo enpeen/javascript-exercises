8ac2971e343f817660df255dee87b964119ead66
enpeen <enpeen0@gmail.com>
2023-06-28 21:36:42 +0200

    Finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,33 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    if (member < 0) {
+        return "OOPS";
+    }
+    if (typeof(member) == "string") {
+        member = parseInt(member);
+    }
+    if (member == 1 || member == 2) {
+        return 1;
+    } 
+    let array = [];
+    let a;
+    let b;
+    let result;
+    for (let i = 1; i <= member - 2; i++) {
+        if (i == 1) {
+            a = i;
+            b = i;
+            result = a + b;
+            array.push(a);
+            array.push(b);
+            array.push(result);
+        } else {
+            a = b;
+            b = result;
+            result = a + b;
+            array.push(result);
+        }
+    }
+    return array[member - 1];
 };
 
 // Do not edit below this line
