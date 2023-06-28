7a8fa7fdbe6096dbb3edb04c5f064ad97cff235a
enpeen <enpeen0@gmail.com>
2023-06-28 19:22:13 +0200

    Maybe revisie and make it more DRY friendly

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,39 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let result = false;
+    let array = string.split("");
+    for (let i = 0; i < array.length; i++) {
+        if (array.includes("!") || array.includes(".")) {
+            let start = array.indexOf("!");
+            array.splice(start, 1);
+        }
+        if (array.includes(".")) {
+            let start = array.indexOf(".");
+            array.splice(start, 1);
+        }
+        if (array.includes(" ")) {
+            let start = array.indexOf(" ");
+            array.splice(start, 1);
+        }
+        if (array.includes(",")) {
+            let start = array.indexOf(",");
+            array.splice(start, 1);
+        }
+    }
+    array = array.map(function(item) {
+        return item.toLowerCase();
+    });
+    const arrayMapped = array.slice();
+    const arrayReversed = arrayMapped.reverse();
+    let counter = -1;
+    for (let i = 0; i < array.length; i++) {
+        if (arrayReversed[i] == array[i]) {
+            counter++;
+        }
+    }
+    if (counter == array.length - 1) {
+            result = true;
+        }
+    return result;
 };
 
 // Do not edit below this line
